f81f94cc887027dfebae02aeee2a4f216f6b5437
raca159 <rafaelcostadealmeida159@gmail.com>
2018-09-18 03:06:17 -0300

    rebuilding checks

--- TxHandler.java
+++ TxHandler.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+
+import Transaction.Input;
+
 import java.util.*;
 
 public class TxHandler {
@@ -16,76 +19,53 @@ public class TxHandler {
   }
 
   /**
-   * @return true if: (1) all outputs claimed by {@code tx} are in the current
-   *         UTXO pool, (2) the signatures on each input of {@code tx} are valid,
-   *         (3) no UTXO is claimed multiple times by {@code tx}, (4) all of
-   *         {@code tx}s output values are non-negative, and (5) the sum of
-   *         {@code tx}s input values is greater than or equal to the sum of its
+   * @return true if:
+   *  (1) all outputs claimed by {@code tx} are in the current UTXO pool,
+   *  (2) the signatures on each input of {@code tx} are valid,
+   *  (3) no UTXO is claimed multiple times by {@code tx},
+   *  (4) all of {@code tx}s output values are non-negative, and
+   *  (5) the sum of {@code tx}s input values is greater than or equal to the sum of its
    *         output values; and false otherwise.
    */
 
   public boolean isValidTx(Transaction tx) {
-    // IMPLEMENT THIS
+
     boolean isValid = true;
 
-    int lengthIns = tx.numInputs();
+    // new transactions made
     ArrayList<UTXO> newUtxos = new ArrayList<UTXO>();
-    for (int i = 0; i < lengthIns; i++) {
-      newUtxos.add(new UTXO(tx.getInput(i).prevTxHash, i));
+    for (int i = 0; i < tx.numInputs(); i++) {
+      newUtxos.add(new UTXO(tx.getInput(i).prevTxHash, tx.getInput(i).outputIndex));
     }
 
-    // hashmap of transactions outputs to see if there is double spending
-    HashMap<UTXO, Integer> doubleSpending = new HashMap<UTXO, Integer>();
-    for (UTXO buffer : newUtxos) {
-      doubleSpending.put(buffer, 0);
-    }
-
-    // check if outputs are in UTXO pool
-    ArrayList<UTXO> allUtxos = ledger.getAllUTXO();
-    // getting all utxos and comparing if new ones already exist
-    for (UTXO bufferUtxo : allUtxos) {
-      for (UTXO bufferNewOnes : newUtxos) {
-        if (bufferNewOnes.compareTo(bufferUtxo) !=0) {
-          doubleSpending.put(bufferNewOnes, doubleSpending.get(bufferNewOnes) + 1);
-        }
+    // check if inputs are available for spending
+    // checking (1)
+    for (UTXO bufferUtxo : newUtxos) {
+      if(!ledger.contains(bufferUtxo)){
+        isValid = false; // means that input are not part of unspent pool
       }
     }
 
-    // // checking (1)
-    // if (doubleSpending.containsValue(1)) {
-    //   isValid = false;
-    // }
-    // // checking (2)
-    // for (Transaction.Input newIns : tx.getInputs()) {
-    //   if (newIns.signature.equals(newIns.prevTxHash)) { // check if each input signature equals to the signature of the
-    //                                                     // block before it
-    //   } else {
-    //     isValid = false;
-    //   }
-    // }
-    // // checking (3)
-    // if (doubleSpending.containsValue(2)) {
-    //   isValid = false;
-    // }
+    // checking if signature of every input is equal to 'hash' of data from previous
+    // checking (2)
+    for (int i = 0; i < tx.numInputs(); i++) {
+      UTXO bufferUtxo = new UTXO(tx.getInput(i).prevTxHash, tx.getInput(i).outputIndex);
+      Transaction.Output bufferOutsUsed = ledger.getTxOutput(bufferUtxo);
+      boolean trueSignature = Crypto.verifySignature(bufferOutsUsed.address, tx.getRawDataToSign(i), tx.getInput(i).signature);
+      if (!trueSignature){
+        isValid = false;
+      }
+    
+    }
+
+    // checking for non negative
     // checking (4) - CORRECT
     for (Transaction.Output bufferOut : tx.getOutputs()) {
       if (bufferOut.value < 0) {
         isValid = false;
       }
     }
-    // checking (5)
-    // double sumIn = 0;
-    // double sumOut = 0;
-    // for (int i = 0; i < tx.numInputs(); i++) {
-    //   UTXO buffers = new UTXO(tx.getInput(i).prevTxHash, i);
-    //   sumIn = sumIn + ledger.getTxOutput(buffers).value;
-    // }
-    // for (Transaction.Output bufferOut : tx.getOutputs()) {
-    //   sumOut = sumOut + bufferOut.value;
-    // }
-    // if (sumIn < sumOut) {
-    //   isValid = false;
-    // }
+
 
     return isValid;
 
